a73cd6705a6763567a06200a4e48b0b5ccfa9c18
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2023-02-22 00:20:20 +0330

    init basic landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, CssBaseline, Paper, Typography, ThemeProvider } from "@mui/material";
+import ResponsiveAppBar from "./ResponsiveAppBar";
+import { createTheme } from "@mui/material/styles";
 
 function App() {
+    // define theme
+    const theme = createTheme({
+      palette: {
+        primary: {
+          light: "#0042AD",
+          main: "#0042AD",
+          dark: "#80a1d6",
+          contrastText: "#001d83",
+        },
+        secondary: {
+          main: "#FD4077",
+          light: "#82e9de",
+          dark: "#00867d",
+          contrastText: "#000",
+        },
+      },
+    });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ResponsiveAppBar />
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Paper
+          elevation={3}
+          sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography color="primary.dark" variant="h1">
+            Starter App
+          </Typography>
+        </Paper>
+      </Box>
+    </ThemeProvider>
   );
 }
 

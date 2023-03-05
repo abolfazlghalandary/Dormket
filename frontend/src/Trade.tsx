2e8b3e9d8adf4fc34f566678cb339385dc9d96a1
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2023-03-06 02:05:25 +0330

    add trade assets

--- frontend/src/Trade.tsx
+++ frontend/src/Trade.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Box, CssBaseline, Paper, Typography, ThemeProvider, Button } from "@mui/material";
+import { Box, CssBaseline, Paper, Typography, ThemeProvider, Divider } from "@mui/material";
 import ResponsiveAppBar from "./ResponsiveAppBar";
 import { createTheme } from "@mui/material/styles";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { ReactComponent as ForgottenCode } from './assets/trade/ForgottenCode.svg';
+import { ReactComponent as Sells } from './assets/trade/Sells.svg';
+import { ReactComponent as Requests } from './assets/trade/Requests.svg';
 import './index.css';
 import OrderBook from './OrderBook';
 
@@ -24,6 +26,18 @@ function Trade() {
                     <OrderBook />
                 </Paper>
             </Box>
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center">
+                <Sells style={{ width: "20vw" }} />
+            </Box >
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center">
+                <Requests style={{ width: "20vw" }} />
+            </Box >
         </>
     );
 }
